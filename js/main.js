f48882ac1bc114dfccc9938ad19bf23fe5f371bc
Ye Yang <yang0237@algonquinlive.com>
2017-05-22 11:05:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var a = 3;
 var b = 5;
 var c;
 
+// alert( "description of alerted_value: " + alerted_value );
 alert("var a = 3;\nvar b = 5;\nvar c;\n_____________\n" +
 	  "a + b = " + (a + b) + "\n" +
 	  "a - b = " + (a - b) + "\n" +
@@ -18,4 +19,23 @@ alert("var a = 3;\nvar b = 5;\nvar c;\n_____________\n" +
 	  "a > b = " + (a > b) + "\n" +
 	  "a < b = " + (a < b) + "\n" +
 	  "!a && !c = " + (!a && !c) + "\n" +
-	  "!a || !c = " + (!a || !c) + "\n");
+	  "!a || !c = " + (!a || !c) + "\n"
+     );
+
+
+//Declare the variable first_name and assign it with the value of your first name;
+var first_name = "Ye";
+//Declare the variable last_name and assign it with the value of your last name;
+var first_name = "Yang";
+//Declare the variable email and assign it with the value of your email;
+var email = "yang0237@algonquinlive.com";
+//Declare the variable output;
+var output;
+//Create the expression concatenating variables first_name, last_name, email and string literals needed to complete the sentence reading: "My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.";
+output = " My name is " + first_name + " " + last_name + "." " + " " + " You can contact me at " + email; 
+
+//Assign output with gotten expression;
+//Print output using alert().
+alert(output);
+
+//
